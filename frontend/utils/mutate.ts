aa2c8166313ec31a72764a26a0e68fe50fa4eb1e
guilhermefront <guilhermesnd14@gmail.com>
2023-02-28 21:27:08 -0300

    feat: added loading to mutations

--- frontend/utils/mutate.ts
+++ frontend/utils/mutate.ts
@@ -1,11 +1,18 @@
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { toast } from 'react-hot-toast';
 
 export const useMutation = () => {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
   const mutate = async (cb) => {
+    setIsLoading(true);
+    const toastId = toast.loading('Loading...');
     await cb();
     router.refresh();
+    toast.dismiss(toastId);
+    setIsLoading(false);
   };
 
-  return { mutate };
+  return { mutate, isLoading };
 };
